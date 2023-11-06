dd45cb5b5261f4ab7b085a46e22b788ec4458f33
Giridhara Prasath Govindarajan <giridharaprasathg@presidio.com>
2023-11-06 12:32:02 +0530

    added gpt playground code

--- app.py
+++ app.py
@@ -17,5 +17,20 @@ def hello_world():
     return {"success": "true", "message": "Hello World!"}, 200
 
 
+@app.route('/api/converter', methods=['POST'])
+def convert():
+    data = request.get_json()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k-0613",
+        messages=data["messages"],
+        temperature=0,
+        max_tokens=2732,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].message["content"]
+
+
 if __name__ == "__main__":
     app.run(port=5000)
