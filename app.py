ccf8c9b64c6f677d646ede647638165009bb187e
Giridhara Prasath Govindarajan <giridharaprasathg@presidio.com>
2023-11-21 08:10:10 +0530

    exceptions handled

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def convert_spl_to_pql():
     if not spl_code or not preset:
         return {'error': 'Missing SPL code or preset'}, 400
 
-    # Prepare the data object using converter.py
-    data = converter.prepare_data(spl_code, preset)
-
     # Call the OpenAI API
     try:
+        # Prepare the data object using converter.py
+        data = converter.prepare_data(spl_code, preset)
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k-0613",
             messages=data["messages"],
@@ -57,26 +57,19 @@ def add_preset():
     messages = request.json.get('messages')
     
     if not name or not messages:
-        return {'success': 'false', 'message': 'Invalid preset name or messages'}, 400
+        return {'success': False, 'message': 'Invalid preset name or messages'}, 400
 
-    constants.add_new_preset(name, messages)
-    return {'success': 'true', 'message': 'New preset added successfully.'}
+    try:
+        constants.add_new_preset(name, messages)
+        return {'success': True, 'message': 'New preset added successfully.'}, 200
+    except Exception as e:
+        return {'success': False, 'message': str(e)}, 500
 
 
 @app.route('/preset', methods=['GET'])
 def get_preset_names():
-    return list(constants.PRESETS.keys())
+    return list(constants.PRESETS.keys()), 200
 
-'''
-@app.route('/reset_presets', methods=['POST'])
-def reset_presets():
-    try:
-        constants.reset_presets_to_base(mode = "soft") 
-        # "soft" OR "hard" - hard reset resets the json file and soft reset does not.
-        return jsonify({'message': 'Presets have been reset to base values.'})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-'''
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
